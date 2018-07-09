422c4845790f4c05ad3b6d14eddd832e48f617d6
chaos <chaos@localhost.localdomain>
2018-07-09 18:12:39 -0400

    pruebapy

---
+++ prueba.py
@@ -0,0 +1,23 @@
+# needs fpdf
+from fpdf import FPDF
+import sys
+import os
+from PIL import Image
+def genpdf(fname,fdir='.'):
+	pdf = FPDF('P','pt',(400,568))
+	flist = ['scan.jpg']
+	for f in flist:
+		im = Image.open(f)
+		print im.size
+		print f
+		pdf.add_page()
+		pdf.set_font('Courier','B')
+		pdf.set_font_size(14)
+		pdf.set_text_color(0,255,0)
+		pdf.text(10,10,'... pwned by truerandom ...')
+		pdf.image(f,0,0)
+	pdf.output(fname, "F")
+	print 'done'
+try: fname = sys.argv[1]
+except: fname = 'output.pdf'
+genpdf(fname)
--- reportlab_genpdf.py
+++ reportlab_genpdf.py
@@ -7,17 +7,16 @@ packet = StringIO.StringIO()
 # read your existing PDF
 existing_pdf = PdfFileReader(file(sys.argv[1], "rb"))
 output = PdfFileWriter()
-# create a new PDF with Reportlab
-print existing_pdf.getPage(0).mediaBox
-can = canvas.Canvas(packet, pagesize=(existing_pdf.getPage(0).mediaBox[2],existing_pdf.getPage(0).mediaBox[3]))
-can.drawString(0, 0, "Hello world")
+# create a new PDF with Reportlab # size : print existing_pdf.getPage(0).mediaBox
+tam = (existing_pdf.getPage(0).mediaBox[2],existing_pdf.getPage(0).mediaBox[3])
+print tam
+can = canvas.Canvas(packet, pagesize=tam)
+can.drawString(0,0,"")
 can.save()
 #move to the beginning of the StringIO buffer
 packet.seek(0)
 new_pdf = PdfFileReader(packet)
-
 # add the "watermark" (which is the new pdf) on the existing page
-#page = existing_pdf.getPage(0)
 page = new_pdf.getPage(0)
 page.mergePage(existing_pdf.getPage(0))
 output.addPage(page)
@@ -25,4 +24,3 @@ output.addPage(page)
 outputStream = file(sys.argv[1]+'.pj', "wb")
 output.write(outputStream)
 outputStream.close()
-
